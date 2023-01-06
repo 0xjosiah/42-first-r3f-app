8a6c3d8d6a24eacf360c0b66e52510ce0b77396b
Josiah Webb <jj.webb520@gmail.com>
2023-01-06 15:58:38 -0700

    adds sphere, torus knot, and plane to scene

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Canvas } from '@react-three/fiber'
+
 export default function App() {
+
     return (
-        <>
-            r3f application
-        </>
+        <Canvas>
+
+            <mesh position={ [2, .75, 0] } >
+                <torusKnotGeometry />
+                <meshNormalMaterial />
+            </mesh>
+
+            <mesh position-x={-2}>
+                <sphereGeometry />
+                <meshBasicMaterial color={0xff1200} />
+            </mesh>
+
+            <mesh position={ [ 0, -1, 0 ] } rotation-x={ Math.PI * -.45 } scale={7} >
+                <planeGeometry />
+                <meshBasicMaterial color={0xfa90fc} />
+            </mesh>
+            
+        </Canvas>
     )
 }
