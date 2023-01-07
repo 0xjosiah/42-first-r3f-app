9c8503a7764deb5be5049aae8e5f17514957e2b2
Josiah Webb <jj.webb520@gmail.com>
2023-01-06 20:20:47 -0700

    messes with canvas settings

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import './styles.css'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Canvas } from '@react-three/fiber'
+import { ACESFilmicToneMapping, CineonToneMapping, LinearEncoding, sRGBEncoding } from 'three'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas
         // orthographic
+        // flat // this refers to tone mapping
+        gl={{
+            // antialias: false,
+            // toneMapping: CineonToneMapping,
+            toneMapping: ACESFilmicToneMapping, // default
+            // outputEncoding: LinearEncoding,
+            outputEncoding: sRGBEncoding, // default
+        }}
         camera={{
             fov: 45,
             // zoom: 100,
